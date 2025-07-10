cbf560a95c89eecc5341abda400ecdab515fb8ab
Suraj-1506 <surajupadhyay442@gmail.com>
2025-07-10 08:15:35 -0700

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,32 +1,35 @@
 const express = require('express');
 
 const app = express();
-
-// app.use("/user",(req,res)=>{
-//     res.send("HAHAHAHAHA")
+// Route: matches "/ac" and "/abc"
+// app.get(/^\/ab?c$/,(req,res)=>{
+//     res.send({firstName:"Suraj",lastName:"Upadhyay"})
 // })
-//this will only handle get call to /user
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Suraj",lastName:"Upadhyay"})
-})
 
-app.post("/user",(req,res)=>{
-    res.send("Data successfully stored in the database.")
-})
+// Route: matches "/abbbc" and "/abc"
+// app.get(/^\/ab+c$/,(req,res)=>{
+//     res.send({firstName:"Suraj",lastName:"Upadhyay"})
+// })
 
-app.delete("/user",(req,res)=>{
-    res.send("Data Deleted successfully!")
-})
+// Route: matches "/abbbc" and "/abc" "ac"
+// app.get(/^\/ab*c$/,(req,res)=>{
+//     res.send({firstName:"Suraj",lastName:"Upadhyay"})
+// })
 
-app.patch("/user",(req,res)=>{
-    res.send("Data updated successfully!")
-})
-app.put("/user",(req,res)=>{
-    res.send("Data updated successfully!")
-})
-//This will match all the http method api call to /test
-app.use("/test",(req,res)=>{  //This function is known as request handler 
-    res.send("Testing the server!")
+//query
+// app.get("/user",(req,res)=>{
+//     console.log(req.query);
+//     res.send({firstName:"Suraj",lastName:"Upadhyay"});
+// })
+
+//params
+// app.get("/user/:userId",(req,res)=>{
+//     console.log(req.params);
+//     res.send({firstName:"Suraj",lastName:"Upadhyay"});
+// })
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({firstName:"Suraj",lastName:"Upadhyay"});
 })
 app.listen(3000,()=>{
     console.log("Server is successfully listning on port 3000...")
