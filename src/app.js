d3cf37108c817241c151566660d287c8701233ef
Suraj-1506 <surajupadhyay442@gmail.com>
2025-07-10 07:34:17 -0700

    exploring get, put, post, patch, and delete.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res)=>{  //This function is known as request handler 
-    res.send("Hello from the server!")
+// app.use("/user",(req,res)=>{
+//     res.send("HAHAHAHAHA")
+// })
+//this will only handle get call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Suraj",lastName:"Upadhyay"})
 })
-app.use("/hello",(req,res)=>{  //This function is known as request handler 
-    res.send("Hello Hello Hello!")
+
+app.post("/user",(req,res)=>{
+    res.send("Data successfully stored in the database.")
 })
+
+app.delete("/user",(req,res)=>{
+    res.send("Data Deleted successfully!")
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("Data updated successfully!")
+})
+app.put("/user",(req,res)=>{
+    res.send("Data updated successfully!")
+})
+//This will match all the http method api call to /test
 app.use("/test",(req,res)=>{  //This function is known as request handler 
     res.send("Testing the server!")
 })
