88b8a14a07a91e6964139e4f39d30ef7a0dbdf19
Suraj-1506 <surajupadhyay442@gmail.com>
2025-07-11 12:27:37 -0700

    writing-auth-middleware.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,42 @@
 const express = require('express');
 
 const app = express();
+const {adminAuth, userAuth} = require("./middlewares/auth")
 
-//app.use("/route", rh1, [rh2, rh3], rh4, 4h5)
-//app.get("/route", rh1, [rh2, rh3], rh4, 4h5)
-app.use(
-    "/user",
-    (req, res, next) => {
-        console.log("Handeling the route user 1");
-        // res.send("Response 1");
-        next();
-        // res.send("Response 1");
-    },
-    (req, res, next) => {
-        console.log("Handeling the route user 2");
-        // res.send("Response 2");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Handeling the route user 3");
-        // res.send("Response 3rd");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Handeling the route user 4");
-        res.send("Response 4");
-        next();
-    }
-)
+//GET /users => It check all the app.xxx("matching route") functions
+// app.use("/",(req, res, next)=>{
+//     // res.send("Handelend / routes");
+//     next()
+// })
+// app.get(
+//     "/user",
+//     (req, res, next) => {
+//         console.log("Handeling /user route");
+//         next();
+//     },
+//     (req, res, next) => {
+//         next();
+//     },
+//     (req, res, next) => {
+//         res.send("2nd route handler");
+//     }
+// )
+
+
+//handle auth middleware for GET,POST
+app.use("/admin",adminAuth)
+app.post("/user/login",(req,res,next)=>{
+    res.send("User loggedin successfully!.")
+})
+app.get("/user/data",userAuth,(req,res,next)=>{
+    res.send("User data Sent.")
+})
+app.get("/admin/getAllData",(req,res,next)=>{
+    res.send("All data Sent")
+})
+app.get("/admin/deleteUser",(req,res,next)=>{
+    res.send("Deleted a user")
+})
 app.listen(3000, () => {
     console.log("Server is successfully listning on port 3000...")
 })
