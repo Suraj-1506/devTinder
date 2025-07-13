3733dcd2cfaed3739cec35a62aac87777293eb00
Suraj-1506 <surajupadhyay442@gmail.com>
2025-07-13 10:07:13 -0700

    Data sanitization and schema validation

--- src/app.js
+++ src/app.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const conncetDB = require("./config/database")
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
+const { validateSignUpData } = require('./utils/validation');
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    // Creating a new instance of User model
-    const user = new User(req.body);
     try {
+        //validation of data
+        validateSignUpData(req);
+        //Encrypt the password
+        const {firstName, lastName, emailId, password} = req.body
+        const passwordHash = await bcrypt.hash(password, 10);
+        console.log(passwordHash)
+        // Creating a new instance of User model
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash
+        });
         await user.save();
         res.send("User added Successfully!!")
     } catch (err) {
-        res.status(400).send("Error saving the user:" + err.message)
+        res.status(400).send("Error :" + err.message)
     }
 
 });
+app.post("/login",async (req,res)=>{
+    try {
+         const {emailId, password} = req.body;
+         const user = await User.findOne({emailId:emailId})
+         if(!user){
+            throw new Error("Invalid Credentials")
+         }
+         const isPasswordValid = await bcrypt.compare(password, user.password)
+         if(isPasswordValid){
+            res.send("Login Successful!!!")
+         }else{
+            throw new Error("Invalid Credentials")
+         }
+    } catch (err) {
+        res.status(400).send("Error :" + err.message)
+    }
+})
 //Get user by email
 app.get("/user", async (req, res) => {
     const userEmail = req.body.emailId;
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,11 +16,21 @@ const userSchema = new mongoose.Schema({
         required:true,
         unique:true,
         lowercase:true,
-        trim:true
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address: "+ value);
+            }
+        }
     },
     password: {
         type: String,
         required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password: "+ value);
+            }
+        }
     },
     age: {
         type: Number,
@@ -35,7 +46,12 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl:{
         type: String,
-        default: "https://www.bing.com/images/search?view=detailV2&ccid=w0TcjC4y&id=3EAEE0D2954752D941E01FEAE1AB90C9F86EE995&thid=OIP.w0TcjC4y9CxTrY3sitYa_AAAAA&mediaurl=https%3a%2f%2fmed.gov.bz%2fwp-content%2fuploads%2f2020%2f08%2fdummy-profile-pic.jpg&exph=400&expw=400&q=dummy+user+full+photo&simid=608044048885174364&FORM=IRPRST&ck=744D5BD6E65CC26A71686A52E2756DE7&selectedIndex=0&itb=0&idpp=overlayview&ajaxhist=0&ajaxserp=0"
+        default: "https://www.bing.com/images/search?view=detailV2&ccid=w0TcjC4y&id=3EAEE0D2954752D941E01FEAE1AB90C9F86EE995&thid=OIP.w0TcjC4y9CxTrY3sitYa_AAAAA&mediaurl=https%3a%2f%2fmed.gov.bz%2fwp-content%2fuploads%2f2020%2f08%2fdummy-profile-pic.jpg&exph=400&expw=400&q=dummy+user+full+photo&simid=608044048885174364&FORM=IRPRST&ck=744D5BD6E65CC26A71686A52E2756DE7&selectedIndex=0&itb=0&idpp=overlayview&ajaxhist=0&ajaxserp=0",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error ("Invalid photo URL: " + value)
+            }
+        }
     },
     about:{
         type: String,
