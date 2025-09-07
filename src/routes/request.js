297658c83145f4afe6a891e21faa3b4bcb9853a1
Suraj-1506 <surajupadhyay442@gmail.com>
2025-09-06 23:14:51 -0700

    connection and request received list apis

--- src/routes/request.js
+++ src/routes/request.js
@@ -48,4 +48,33 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
     }
 });
 
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req,res)=>{
+    try {
+        const loggedInUser = req.user;
+        const { status, requestId } = req.params;
+        //validate the status
+        console.log(status,requestId,loggedInUser._id);
+        const allowedStatus = ["accepted",'rejected']
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message: "Status not allowed!"})
+        }
+        //loggedInId = toUserId
+        //status = interested
+        //request Id should be valid
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested",
+        });
+        console.log("connectionRequest",connectionRequest)
+        if(!connectionRequest){
+            return res.status(404).json({message: "Connection request not found!"})
+        }
+        connectionRequest.status = status;
+        const data = await connectionRequest.save();
+        res.json({message:`Connection request ${status}`,data})
+    } catch (err) {
+        res.status(400).send("Error :" + err.message)
+    }
+})
 module.exports = requestRouter;
